cad307e9cded8e4c983572f4bfd027c6099c316f
lucaskapobassis <lkap4650@gmail.com>
2022-07-31 12:37:08 +1000

    added config.ini in main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+rbxInfo = config["roblox"]
+discordInfo = config["bot"]
+
+dsKey = rbxInfo["ds_key"]
+dsPrefix = rbxInfo["ds_prefix"]
+dsUniverse = rbxInfo["ds_universe"]
+dsToken = rbxInfo["ds_token"]
+
+botToken = discordInfo["token"]
+
+print(botToken)
